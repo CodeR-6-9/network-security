122f70a3a6d377d6a2e11c6ba39e7eae7bf98857
Hridesh <hridesh.mehrotra@gmail.com>
2026-08-21 20:20:20 +0530

    training, evaluation and mlflow completed

--- main.py
+++ main.py
@@ -3,9 +3,10 @@ import sys
 from networksecurity.components.data_ingestion import DataIngestion
 from networksecurity.components.data_transformation import DataTransformation
 from networksecurity.components.data_validation import DataValidation
+from networksecurity.components.model_trainer import ModelTrainer
 from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logging.logger import logging
-from networksecurity.entity.config_entity import DataIngestionConfig, DataTransformationConfig, DataValidationConfig, TrainingPipelineConfig
+from networksecurity.entity.config_entity import DataIngestionConfig, DataTransformationConfig, DataValidationConfig, ModelTrainerConfig, TrainingPipelineConfig
 
 if __name__=='__main__':
     try:
@@ -16,18 +17,29 @@ if __name__=='__main__':
         dataingestionartifact=data_ingestion.initiate_data_ingestion()
         logging.info("Data ingestion completed")
         print(dataingestionartifact)
+
+
         data_validation_config=DataValidationConfig(trainingpipelineconfig)
         data_validation=DataValidation(dataingestionartifact,data_validation_config)
         logging.info("Initiating the data validation")
         data_validation_artifact=data_validation.initiate_data_validation()
         logging.info("Data validation completed")
         print(data_validation_artifact)
+
+
         data_transformation_config=DataTransformationConfig(trainingpipelineconfig)
         logging.info("Initiating the data transformation")
         data_transformation=DataTransformation(data_validation_artifact,data_transformation_config)
         data_transformation_artifact=data_transformation.initiate_data_transformation()
         logging.info("Data transformation completed")
         print(data_transformation_artifact)
+
+        model_trainer_config=ModelTrainerConfig(trainingpipelineconfig)
+        logging.info("Initiating the model trainer")
+        model_trainer=ModelTrainer(model_trainer_config,data_transformation_artifact)
+        model_trainer_artifact=model_trainer.initiate_model_trainer()
+        logging.info("Model trainer completed")
+        print(model_trainer_artifact)
 
     except Exception as e:
         raise NetworkSecurityException(e,sys)
--- networksecurity/utils/main_utils/utils.py
+++ networksecurity/utils/main_utils/utils.py
@@ -7,7 +7,7 @@ import numpy as np
 import pickle
 
 from sklearn.metrics import r2_score
-from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import GridSearchCV, RandomizedSearchCV
 
 def read_yaml_file(file_path: str) -> dict:
     try:
@@ -83,10 +83,11 @@ def evaluate_models(X_train, y_train,X_test,y_test,models,param):
             model = list(models.values())[i]
             para=param[list(models.keys())[i]]
 
-            gs = GridSearchCV(model,para,cv=3)
-            gs.fit(X_train,y_train)
+            #gs = GridSearchCV(model,para,cv=3)
+            rs = RandomizedSearchCV(model,para,cv=3)
+            rs.fit(X_train,y_train)
 
-            model.set_params(**gs.best_params_)
+            model.set_params(**rs.best_params_)
             model.fit(X_train,y_train)
 
             #model.fit(X_train, y_train)  # Train model
